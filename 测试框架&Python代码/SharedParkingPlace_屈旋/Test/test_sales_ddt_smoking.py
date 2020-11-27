c76690af1e461fefa122f60832f6d9e07454f698
qx <1378187065@qq.com>
2020-11-27 11:20:38 +0800

    add

--- 测试框架&Python代码/SharedParkingPlace_屈旋/Test/test_sales_ddt_smoking.py
+++
@@ -1,8 +0,0 @@
-from FrameDemo.SharedParkingPlace.Kw_driver.ddt_start import Start
-
-class sales_ddt:
-    def start(self):
-        Start().run()
-
-if __name__ == '__main__':
-    sales_ddt().start()
